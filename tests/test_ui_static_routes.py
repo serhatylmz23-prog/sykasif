77f21a506088519bfd7ddcbcd037a61a9ede1f9d
serhatylmz23-prog <serhatylmz23@hotmail.com>
2026-08-02 22:30:43 +0300

    test(ui): verify static route registration

---
+++ tests/test_ui_static_routes.py
@@ -0,0 +1,16 @@
+﻿"""
+SPR-003-UI-0016
+SyKaşif Static Routes Test
+"""
+
+from fastapi import FastAPI
+
+from syk_ui.static_routes import mount_static
+
+
+def test_ui_static_route_registration():
+    app = FastAPI()
+
+    mount_static(app)
+
+    assert any(route.path == "/syk-ui" for route in app.routes)
