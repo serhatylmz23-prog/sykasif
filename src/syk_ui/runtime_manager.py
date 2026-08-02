c323a289cd97c95b0eb1378c1847623c01335893
serhatylmz23-prog <serhatylmz23@hotmail.com>
2026-08-02 16:58:44 +0300

    feat(ui): add runtime manager

---
+++ src/syk_ui/runtime_manager.py
@@ -0,0 +1,31 @@
+﻿"""
+SPR-003-UI-0004
+SyKaşif Runtime Manager
+"""
+
+from .module_registry import MODULES
+
+
+class RuntimeManager:
+
+    def __init__(self):
+        self._modules = {m.id: m for m in MODULES}
+        self._active = "dashboard"
+
+    @property
+    def active(self):
+        return self._modules[self._active]
+
+    def activate(self, module_id: str):
+
+        if module_id not in self._modules:
+            raise KeyError(f"Module not found: {module_id}")
+
+        self._active = module_id
+        return self.active
+
+    def modules(self):
+        return list(self._modules.values())
+
+    def exists(self, module_id: str) -> bool:
+        return module_id in self._modules
